2ead1c328774a6828a714f3c4827f8fce05a1cb7
Glenn ten Cate <glenntencate@gmail.com>
2019-12-03 15:36:13 +0100

    Update __init__.py

--- target/project/__init__.py
+++ target/project/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 __version__ = '0.1'
 from flask import Flask
+import getpass
+
 app = Flask('project')
 app.config['SECRET_KEY'] = 'random'
+app.config['username'] = getpass.getuser()
 app.debug = True
 from project.controllers import *
